fe73d0b1b71e12181facd45b083a14061bf78310
goodeeit <gt_min@naver.com>
2022-08-01 17:45:27 +0900

    2022-08-01

--- javastudy/06_INHERITANCE/src/quiz04_cart/Customer.java
+++ javastudy/06_INHERITANCE/src/quiz04_cart/Customer.java
@@ -5,6 +5,7 @@ public class Customer {
 	// 필드
 	private int money;
 	private int bonusPoint;
+	private int total;
 	private Product[] cart = new Product[10];
 	private int idx;  // cart에 담긴 Product의 개수. cart 배열의 인덱스.
 
@@ -30,7 +31,7 @@ public class Customer {
 		int price = product.getPrice();
 		// 가진 돈보다 비싼 물건은 못 산다.
 		if(money < price) {
-			System.out.println("돈이 " + (price - money) + "원 부족합니다.");
+			System.out.println(product.getName() + " 사려면 돈이 " + (price - money) + "원 부족합니다.");
 			return;
 		}
 		// 카트가 가득 차면 물건을 못 산다.
@@ -41,22 +42,23 @@ public class Customer {
 		// 구매
 		cart[idx++] = product;
 		money -= price;
+		total += price;
 		bonusPoint += price * 0.1;
 	}
 	
 	// receipt() 메소드
 	public void receipt() {
+		System.out.println();
+		System.out.println("===== 영수증 =====");
 		// 물건을 안 샀다.
 		if(idx == 0) {
 			System.out.println("구매한 물건이 없습니다.");
 			return;
 		}
 		// 구매 총액 구하기 및 출력
-		int total = 0;
 		for(int i = 0; i < idx; i++) {
 			Product product = cart[i];
 			System.out.println(product.getName() + "  " + product.getPrice() + "원");
-			total += product.getPrice();
 		}
 		System.out.println("-----------------------");
 		System.out.println("구매총액 " + total + "원");
