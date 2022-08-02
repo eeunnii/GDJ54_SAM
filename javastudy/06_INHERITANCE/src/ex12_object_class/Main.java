b6f4d94f3f6f7d89d32561fd97d635dde3093668
goodeeit <gt_min@naver.com>
2022-08-02 10:50:27 +0900

    2022-08-02

---
+++ javastudy/06_INHERITANCE/src/ex12_object_class/Main.java
@@ -0,0 +1,18 @@
+package ex12_object_class;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Object 클래스는 모든 객체(변수)를 저장할 수 있다.
+		Object p = new Person();
+		
+		// Object 클래스타입의 객체는
+		// 항상 다운캐스팅해서 사용해야 한다.
+		if(p instanceof Person) {
+			((Person) p).eat();
+		}
+
+	}
+
+}
---
+++ javastudy/06_INHERITANCE/src/ex12_object_class/Person.java
@@ -0,0 +1,9 @@
+package ex12_object_class;
+
+public class Person {
+
+	public void eat() {
+		System.out.println("먹는다.");
+	}
+	
+}
