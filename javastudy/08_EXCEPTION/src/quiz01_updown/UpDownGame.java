73ad8a248d8256b4b2448cf55e7a31421c9daeaf
goodeeit <gt_min@naver.com>
2022-08-04 12:51:40 +0900

    2022-08-04

--- javastudy/08_EXCEPTION/src/quiz01_updown/UpDownGame.java
+++ javastudy/08_EXCEPTION/src/quiz01_updown/UpDownGame.java
@@ -1,5 +1,6 @@
 package quiz01_updown;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UpDownGame {
@@ -11,23 +12,45 @@ public class UpDownGame {
 	
 	// 생성자
 	public UpDownGame() {
-		// rand, sc 만들기
-		
+		rand = (int)(Math.random() * 100 + 1);
+		sc = new Scanner(System.in);
 	}
 	
 	// 입력
 	public int input() {
-		
-		return 0;
+		try {
+			count++;
+			System.out.print("입력 >>> ");
+			int n = sc.nextInt();
+			if(n < 1 || n > 100) {
+				throw new RuntimeException("1 ~ 100 사이 정수만 입력할 수 있습니다.");
+			}
+			return n;
+		} catch (InputMismatchException e) {
+			System.out.println("정수만 입력할 수 있습니다.");
+			sc.next();  // 잘못 입력된 문자열 먹어 치우기
+			input();
+		} catch(RuntimeException e) {
+			System.out.println(e.getMessage());
+			input();
+		}
+		return 0;  // 이클립스 안심시키는 용도
 	}
 	
 	// 실행
 	public void play() {
 	
-		// 맞힐때까지 무한루프
-
-		int n = input();
-		
+		while(true) {
+			int n = input();
+			if(n < rand) {
+				System.out.println("Up!");
+			} else if (n > rand) {
+				System.out.println("Down!");
+			} else {
+				System.out.println(rand + " 정답입니다. " + count + "번만에 정답");
+				break;
+			}
+		}
 		
 	}
 	
