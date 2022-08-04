76c1c8e449f000b06436cd2611e00d40076a6cab
goodeeit <gt_min@naver.com>
2022-08-04 12:07:17 +0900

    2022-08-04

---
+++ javastudy/08_EXCEPTION/src/quiz01_updown/UpDownGame.java
@@ -0,0 +1,34 @@
+package quiz01_updown;
+
+import java.util.Scanner;
+
+public class UpDownGame {
+
+	// 필드
+	private int rand;   // 1 ~ 100 사이 난수
+	private int count;  // 시도 횟수
+	private Scanner sc;
+	
+	// 생성자
+	public UpDownGame() {
+		// rand, sc 만들기
+		
+	}
+	
+	// 입력
+	public int input() {
+		
+		return 0;
+	}
+	
+	// 실행
+	public void play() {
+	
+		// 맞힐때까지 무한루프
+
+		int n = input();
+		
+		
+	}
+	
+}
