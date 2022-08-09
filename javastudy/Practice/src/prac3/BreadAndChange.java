b7a5e8b4e99fbf3155ed59441aca8bcfdd51eae9
goodeeit <gt_min@naver.com>
2022-08-09 15:51:34 +0900

    2022-08-09

--- javastudy/Practice/src/prac3/BreadAndChange.java
+++ javastudy/Practice/src/prac3/BreadAndChange.java
@@ -6,4 +6,28 @@ public class BreadAndChange {
 	private int bread;   // 빵
 	private int change;  // 잔돈
 	
+	// 생성자
+	public BreadAndChange() {
+		
+	}
+	public BreadAndChange(int bread, int change) {
+		super();
+		this.bread = bread;
+		this.change = change;
+	}
+	
+	// 메소드
+	public int getBread() {
+		return bread;
+	}
+	public void setBread(int bread) {
+		this.bread = bread;
+	}
+	public int getChange() {
+		return change;
+	}
+	public void setChange(int change) {
+		this.change = change;
+	}
+	
 }
--- javastudy/Practice/src/prac3/Customer.java
+++ javastudy/Practice/src/prac3/Customer.java
@@ -7,11 +7,22 @@ public class Customer {
 	private int cnt;    // 구매한 빵개수
 	
 	// 생성자
-	
+	public Customer(int money) {
+		this.money = money;
+	}
 	
 	// 구매 (구매 후 출력)
 	// Bakery에서 판매한 빵을 가질 수 있다.(Bakery의 sell() 메소드 사용)
 	public void buy(Bakery bakery, int buyMoney) {
+		
+		BreadAndChange bnc = bakery.sell(buyMoney);
+		
+		cnt += bnc.getBread();
+		money += bnc.getChange();
+		
+		money -= buyMoney;
+		
+		System.out.println("구매한빵 " + cnt + "개, 남은돈 " + money + "원");
 		
 	}
 	
