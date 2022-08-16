5d56186e0959100955c856516a59ff2a3477d485
goodeeit <gt_min@naver.com>
2022-08-16 09:20:34 +0900

    2022-08-16

--- javastudy/11_NETWORK/src/prac1/Main.java
+++ javastudy/11_NETWORK/src/prac1/Main.java
@@ -17,7 +17,7 @@ public class Main {
 		try {
 		
 			// 접속
-			String apiURL = "https://kma.go.kr/XML/weather/sfc_web_map.xml";
+			String apiURL = "https://www.kma.go.kr/XML/weather/sfc_web_map.xml";
 			URL url = new URL(apiURL);
 			HttpURLConnection con = (HttpURLConnection)url.openConnection();
 			
@@ -47,6 +47,7 @@ public class Main {
 		} catch(MalformedURLException e) {
 			System.out.println("API 주소 오류");
 		} catch(IOException e) {
+			System.out.println(e.getMessage());
 			System.out.println("API 서버 오류");
 		}
 
