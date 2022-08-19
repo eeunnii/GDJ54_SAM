b63348a7c534f4ee5cc8e1d3a54d980c3c98cf41
goodeeit <gt_min@naver.com>
2022-08-19 12:50:38 +0900

    2022-08-19

--- javastudy/12_NETWORK/src/ex02_api/Main2_JSON.java
+++ javastudy/12_NETWORK/src/ex02_api/Main2_JSON.java
@@ -13,6 +13,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Main2_JSON {
@@ -144,8 +145,8 @@ public class Main2_JSON {
 			urlBuilder.append("&numOfRows=10");
 			urlBuilder.append("&pageNo=1");
 			urlBuilder.append("&dataType=JSON");
-			urlBuilder.append("&base_date=20220818");
-			urlBuilder.append("&base_time=1100");
+			urlBuilder.append("&base_date=20220819");
+			urlBuilder.append("&base_time=0600");
 			urlBuilder.append("&nx=58");   // 서울특별시 금천구 가산동 좌표X
 			urlBuilder.append("&ny=125");  // 서울특별시 금천구 가산동 좌표Y
 		} catch (UnsupportedEncodingException e) {
@@ -212,15 +213,109 @@ public class Main2_JSON {
 	}
 	
 	public static void m4() {
-		
-		String response = m3();
-		System.out.println(response);
+
+		JSONObject obj = new JSONObject(m3());
+		JSONObject response = obj.getJSONObject("response");
+		JSONObject body = response.getJSONObject("body");
+		JSONObject items = body.getJSONObject("items");
+		JSONArray item = items.getJSONArray("item");
+		for(int i = 0; i < item.length(); i++) {
+			JSONObject element = item.getJSONObject(i);
+			System.out.println(element.getString("category") + " : " + element.getString("obsrValue"));
+		}
 		
 	}
 	
+	public static String m5() {
+		
+		// 소상공인시장진흥공단_상가(상권)정보
+		
+		// 인증키(Decoding)
+		String serviceKey = "bEQBRPHjt0tZrc7EsL0T8usfsZ1+wT+5jqamBef/ErC/5ZO6N7nYdRmrwR91bh5d3I1AQeY5qdbJOF6Kv0U1CQ==";
+
+		// API 주소	(주소 + 요청 파라미터)
+		StringBuilder urlBuilder = new StringBuilder();
+		try {
+			urlBuilder.append("http://apis.data.go.kr/B553077/api/open/sdsc2/storeZoneOne");
+			urlBuilder.append("?serviceKey=").append(URLEncoder.encode(serviceKey, "UTF-8"));
+			urlBuilder.append("&key=9940");
+			urlBuilder.append("&type=json");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		String apiURL = urlBuilder.toString();
+		
+		// API 주소 접속
+		URL url = null;
+		HttpURLConnection con = null;
+		
+		try {			
+			url = new URL(apiURL);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+		} catch(MalformedURLException e) {
+			System.out.println("API 주소 오류");
+		} catch(IOException e) {
+			System.out.println("API 접속 실패");
+		}
+		
+		// 입력 스트림 생성
+		// 1. 서버가 보낸 데이터를 읽어야 하므로 입력 스트림이 필요
+		// 2. 서버와 연결된 입력 스트림은 바이트 스트림이므로 문자 스트림으로 변환해야 함
+		BufferedReader reader = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			
+			if(con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			
+			String line = null;
+			while((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+			
+			// 스트림 종료
+			reader.close();
+			
+		} catch(IOException e) {
+			System.out.println("API 응답 실패");
+		}
+		
+		// API로부터 전달받은 json 데이터
+		String response = sb.toString();
+		
+		// JSON File 생성
+		File file = new File("C:\\storage", "api3.json");
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+			bw.write(response);
+			bw.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return response;
+		
+	}
+	
+	public static void m6() {
+		
+		JSONObject obj = new JSONObject(m5());
+		JSONObject header = obj.getJSONObject("header");
+		JSONArray columns = header.getJSONArray("columns");
+		for(int i = 0; i < columns.length(); i++) {
+			System.out.println(columns.getString(i));
+		}
+		
+	}
 	
 	public static void main(String[] args) {
-		m4();
+		m6();
 	}
 
 }
