53ab142e11b4b7ddc9dc10a70d186e2c0d6e3eed
goodeeit <gt_min@naver.com>
2022-08-23 19:50:35 +0900

    2022-08-23

--- javastudy/ReExam03/src/api/Main.java
+++ javastudy/ReExam03/src/api/Main.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
 
@@ -38,15 +39,44 @@ public class Main {
 				sb.append(line);
 			}
 			
-			// StringBuilder에 저장된 응답(XML) 데이터를 JSON으로 변경하기
-			JSONObject obj = XML.toJSONObject(sb.toString());
-			System.out.println(obj);
-			
+			// 파싱 결과를 저장할 파일
 			File file = new File("test.txt");
 			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-		
-		
-
+			
+			// StringBuilder에 저장된 응답(XML) 데이터를 JSON으로 변경하기
+			JSONObject obj = XML.toJSONObject(sb.toString());
+			JSONObject rss = obj.getJSONObject("rss");
+			JSONObject channel = rss.getJSONObject("channel");
+			String link = channel.getString("link");
+			String description = channel.getString("description");
+			String generator = channel.getString("generator");
+			String language = channel.getString("language");
+			String title = channel.getString("title");
+			String pubDate = channel.getString("pubDate");
+			bw.write(pubDate + "\n");
+			JSONObject item = channel.getJSONObject("item");
+			String author = item.getString("author");
+			String link2 = item.getString("link");
+			String category = item.getString("category");
+			bw.write(category + "\n");
+			String title2 = item.getString("title");
+			JSONObject description2 = item.getJSONObject("description");
+			JSONObject body = description2.getJSONObject("body");
+			JSONArray dataList = body.getJSONArray("data");
+			for(int i = 0; i < dataList.length(); i++) {
+				JSONObject data = dataList.getJSONObject(i);
+				int sky = data.getInt("sky");
+				int temp = data.getInt("temp");
+				String wfKor = data.getString("wfKor");
+				int wd = data.getInt("wd");
+				int hour = data.getInt("hour");
+				bw.write(hour + "시, " + temp + "도, " + wfKor + "\n");
+			}
+			
+			bw.close();
+			br.close();
+			con.disconnect();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
