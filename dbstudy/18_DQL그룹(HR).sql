44c6b9e328c3d41b5c4155ae300c8a39ff9512c2
gt_min@naver.com <tktlsp1@gmail.com>
2022-08-30 14:47:31 +0900

    2022-08-30

--- dbstudy/18_DQL그룹(HR).sql
+++ dbstudy/18_DQL그룹(HR).sql
@@ -80,20 +80,37 @@ HAVING
 -- EMPLOYEES 테이블 연습.
 
 -- 1. 급여평균이 10000 이상인 부서의 부서번호와 급여평균을 조회하기
+SELECT DEPARTMENT_ID, FLOOR(AVG(SALARY))
+  FROM EMPLOYEES
+ GROUP BY DEPARTMENT_ID
+HAVING AVG(SALARY) >= 10000;
 
-
+ 
 -- 2. 동일한 부서번호(DEPARTMENT_ID)로 그룹화하기
 --    동일한 부서번호를 가진 사원들을 직업아이디(JOB_ID)로 다시 그룹화하기
 --    즉, 부서별 직업아이디별로 그룹화하기
 --    부서별 직업아이디별로 그룹화하여 각 그룹의 사원수 조회하기
 --    부서번호가 없는 사원은 제외하기
+SELECT DEPARTMENT_ID, JOB_ID, COUNT(*) AS 사원수
+  FROM EMPLOYEES
+ WHERE DEPARTMENT_ID IS NOT NULL
+ GROUP BY DEPARTMENT_ID, JOB_ID
+ ORDER BY 사원수;
 
 
 -- DEPARTMENTS 테이블 연습.
 
 -- 1. 동일한 지역(LOCATION_ID)으로 그룹화하여 조회하기
+SELECT LOCATION_ID
+  FROM DEPARTMENTS
+ GROUP BY LOCATION_ID;
 
 -- 2. 동일한 지역(LOCATION_ID)으로 그룹화하여 각 지역별 존재하는 부서수 조회하기
+--    부서수가 2 이상인 지역만 조회하기
+SELECT LOCATION_ID, COUNT(*) AS 부서수
+  FROM DEPARTMENTS
+ GROUP BY LOCATION_ID
+HAVING COUNT(*) >= 2;
 
 -- 3. 동일한 지역(LOCATION_ID)으로 그룹화하여 각 지역별 존재하는 부서수 조회하기
 --    MANAGER_ID가 없는 지역은 제외하고 조회하기
