b6abd0e0c11a2681c2261e9b762b2f76090db519
gt_min@naver.com <tktlsp1@gmail.com>
2022-09-01 14:44:04 +0900

    2022-09-01

--- dbstudy/22_뷰(부서,사원).sql
+++ dbstudy/22_뷰(부서,사원).sql
@@ -11,6 +11,7 @@
        CREATE VIEW 뷰_이름 AS (쿼리문)
 */
 
+
 -- 1. 뷰 생성
 CREATE VIEW VIEW_EMP
     AS (SELECT E.EMP_NO, E.NAME, E.DEPART, D.DEPT_NAME, E.GENDER, E.POSITION, E.HIRE_DATE, E.SALARY
@@ -19,3 +20,12 @@ CREATE VIEW VIEW_EMP
 
 -- 2. 뷰 확인
 SELECT * FROM VIEW_EMP;
+
+-- 3. 뷰 삭제
+DROP VIEW VIEW_EMP;
+
+-- 사용자가 작성한 VIEW 확인하려면 USER_VIEWS 데이터 사전을 확인
+DESC USER_VIEWS;
+SELECT VIEW_NAME  -- 뷰 이름
+     , TEXT       -- 저장된 쿼리문
+  FROM USER_VIEWS;
