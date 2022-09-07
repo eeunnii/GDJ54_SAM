4d811b243e94996e896b03bd3ae78f86a0acfe0e
gt_min@naver.com <tktlsp1@gmail.com>
2022-09-07 15:50:12 +0900

    2022-09-07

--- javastudy/14_JDBC/src/repository/ContactDAO.java
+++ javastudy/14_JDBC/src/repository/ContactDAO.java
@@ -5,15 +5,18 @@ public class ContactDAO {
 	// DAO는 하나의 객체만 생성할 수 있도록
 	// Singleton Pattern으로 생성
 	
+	// Singleton 패턴 - 1
 	// ContactDAO 객체를 하나 만들어 둔다.
 	private ContactDAO contactDAO = new ContactDAO();
 	
+	// Singleton 패턴 - 2
 	// 외부에서는 ContactDAO 객체를 못 만들도록 제한한다.
-	// private 생성자
+	// private 생성자를 이용한다.
 	private ContactDAO() {
 		
 	}
 	
+	// Singleton 패턴 - 3
 	// 만들어 둔 ContactDAO 객체를 외부에 반환한다.
 	public ContactDAO getInstance() {
 		return contactDAO;
