3d670e3fe64dea65d2c443791d9931e6f9c42fe8
gt_min@naver.com <gt_min@naver.com>
2022-10-06 15:19:32 +0900

    2022-10-06

--- jspstudy/01_Servlet/src/main/java/ex02/MyServlet.java
+++ jspstudy/01_Servlet/src/main/java/ex02/MyServlet.java
@@ -1,6 +1,8 @@
 package ex02;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -59,13 +61,33 @@ public class MyServlet extends HttpServlet {
 		// 1) MIME-TYPE + 2) 문자셋
 		response.setContentType("text/html; charset=UTF-8");
 		
+		// 3) 응답 스트림 생성
+		//    (1) 문자 출력 스트림(*Writer)을 생성
+		//    (2) response 객체로부터 PrintWriter 객체를 얻을 수 있음
+		//        - IOException 예외 처리가 필요한데 이미 처리되어 있음
+		//        - write() 메소드보다는 print()/println() 메소드를 사용하는 것이 권장
+		PrintWriter out = response.getWriter();
 		
+		// 4) 응답 만들기 (HTML 문서 만들기)
+		out.println("<html lang=\"ko\">");
+		out.println("<head>");
+		out.println("<meta charset=\"UTF-8\">");
+		out.println("<title>");
+		out.println("나의 첫 번째 응답");
+		out.println("</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>안녕하세요. " + name + "님 반갑습니다 ♥</h1>");
+		if(age >= 20) {
+			out.println("<h1>귀하는 " + age + "살이므로 입장이 가능합니다.</h1>");
+		} else {
+			out.println("<h1>" + age + "살? 애들은 다음에</h1>");
+		}
+		out.println("</body>");
+		out.println("</html>");
 		
-		
-		
-		
-		
-		
+		out.flush();  // 출력 스트림에 남아 있는 모든 데이터 내보내기 (만약을 위해서)
+		out.close();
 		
 	}
 
