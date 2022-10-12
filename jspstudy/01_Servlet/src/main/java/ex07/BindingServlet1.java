fc9a0f57b3a6c939f4068d6b8d113a566f01b844
gt_min@naver.com <gt_min@naver.com>
2022-10-12 11:36:14 +0900

    2022-10-12

--- jspstudy/01_Servlet/src/main/java/ex07/BindingServlet1.java
+++ jspstudy/01_Servlet/src/main/java/ex07/BindingServlet1.java
@@ -1,6 +1,7 @@
 package ex07;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -30,7 +31,7 @@ public class BindingServlet1 extends HttpServlet {
 		// 2. 3개 영역을 사용
 		//    1) ServletContext     : 애플리케이션(프로젝트) 종료 전까지 접근해서 사용 가능
 		//    2) HttpSession        : 브라우저 종료 전까지 접근해서 사용 가능
-		//    3) HttpServletRequest : 응답 전까지 접근해서 사용 가능
+		//    3) HttpServletRequest : 하나의 요청/응답 사이클 내에서 접근해서 사용 가능
 		// 3. 사용 메소드
 		//    1) getAttribute('속성')     : 해당 속성 값 가져오기 (캐스팅이 필요할 수 있음)
 		//    2) setAttribute('속성', 값) : 속성에 값 저장하기    (Object 타입으로 저장)
@@ -50,6 +51,24 @@ public class BindingServlet1 extends HttpServlet {
 		// HttpServletRequest
 		request.setAttribute("c", 3);
 		
+		
+		// 응답
+		/*
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<a href=\"/01_Servlet/BindingServlet2\">이동</a>");
+		out.close();
+		*/
+		
+		// 포워드
+		// 1. request를 전달함
+		// 2. 컨텍스트 내부 이동이므로 경로 작성 시 컨텍스트 패스는 작성하지 않음
+		// request.getRequestDispatcher("/BindingServlet2").forward(request, response);
+		
+		// 리다이렉트
+		// 1. request를 전달하지 않음
+		// 2. 클라이언트 -> 서버로 이동하므로 컨텍스트 패스를 작성해야 함
+		response.sendRedirect("/01_Servlet/BindingServlet2");
 	
 	}
 
