3781c30170a02700e16ba277fea3d7f71ce7a328
gt_min@naver.com <gt_min@naver.com>
2022-10-18 10:27:58 +0900

    2022-10-18

---
+++ jspstudy/04_Dbcp/src/main/java/controller/BoardController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.ActionForward;
+import service.BoardService;
+
+@WebServlet("*.do")
+
+public class BoardController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// 요청 / 응답 인코딩
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		// 요청 확인
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String urlMapping = requestURI.substring(contextPath.length() + 1);
+		
+		// BoardService 선언
+		BoardService service = null;
+		
+		// ActionForward 선언
+		ActionForward af = null;
+		
+		// 요청(urlMapping)에 따른 Service 생성
+		switch(urlMapping) {
+		
+		}
+		
+		// Service 실행
+		if(service != null) {
+			af = service.execute(request, response);
+		}
+		
+		// 어디로 어떻게?
+		if(af != null) {
+			if(af.isRedirect()) {
+				response.sendRedirect(af.getView());
+			} else {
+				request.getRequestDispatcher(af.getView()).forward(request, response);
+			}
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
