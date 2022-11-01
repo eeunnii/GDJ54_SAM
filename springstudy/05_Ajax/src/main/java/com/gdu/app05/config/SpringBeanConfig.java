8a97cd5c8ef5ac99be233d73d520399a9f4c917c
gt_min@naver.com <gt_min@naver.com>
2022-11-02 08:51:16 +0900

    2022-11-02

--- springstudy/05_Ajax/src/main/java/com/gdu/app05/config/SpringBeanConfig.java
+++ springstudy/05_Ajax/src/main/java/com/gdu/app05/config/SpringBeanConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 
 import com.gdu.app05.service.BoardService;
 import com.gdu.app05.service.BoardServiceImpl;
+import com.gdu.app05.service.ContactService;
+import com.gdu.app05.service.ContactServiceImpl;
 
 @Configuration
 public class SpringBeanConfig {
@@ -12,6 +14,11 @@ public class SpringBeanConfig {
 	@Bean
 	public BoardService boardService() {
 		return new BoardServiceImpl();
+	}
+	
+	@Bean
+	public ContactService contactService() {
+		return new ContactServiceImpl();
 	}
 	
 }
