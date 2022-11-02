c3951a356066673a769a9d9186ad27de25911dce
gt_min@naver.com <gt_min@naver.com>
2022-11-02 18:54:29 +0900

    2022-11-02

---
+++ jspstudy/myhome/src/main/java/service/NoticeServiceImpl.java
@@ -0,0 +1,15 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import common.ActionForward;
+
+public class NoticeServiceImpl implements NoticeService {
+
+	@Override
+	public ActionForward findAllNotices(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
