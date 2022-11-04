a8139ae1affb15ebbccb3d75f3f71ecb2862ddac
gt_min@naver.com <gt_min@naver.com>
2022-11-04 14:14:02 +0900

    2022-11-04

--- springstudy/08_SpringJdbc/src/main/java/com/gdu/app08/repository/BoardDAO.java
+++ springstudy/08_SpringJdbc/src/main/java/com/gdu/app08/repository/BoardDAO.java
@@ -1,10 +1,13 @@
 package com.gdu.app08.repository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Repository;
 
 import com.gdu.app08.domain.BoardDTO;
@@ -31,17 +34,26 @@ public class BoardDAO {
 		return board;
 	}
 	
-	public int insertBoard(BoardDTO board) {
+	public int insertBoard(final BoardDTO board) {
+		String sql = "INSERT INTO BOARD(BOARD_NO, TITLE, CONTENT, WRITER, CREATE_DATE, MODIFY_DATE) "
+		           + "VALUES(BOARD_SEQ.NEXTVAL, ?, ?, ?, TO_CHAR(SYSDATE, 'YYYY-MM-DD'), TO_CHAR(SYSDATE, 'YYYY-MM-DD'))";
+		int result = jdbcTemplate.update(sql, new PreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				ps.setString(1, board.getTitle());
+				ps.setString(2, board.getContent());
+				ps.setString(3, board.getWriter());
+			}
+		});
+		return result;
+	}
+	
+	public int updateBoard(final BoardDTO board) {
 		
 		return 0;
 	}
 	
-	public int updateBoard(BoardDTO board) {
-		
-		return 0;
-	}
-	
-	public int deleteBoard(int board_no) {
+	public int deleteBoard(final int board_no) {
 		
 		return 0;
 	}
