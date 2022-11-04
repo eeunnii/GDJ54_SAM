3040ba70ae31e95b92e59bde67f070362bdb7ec6
gt_min@naver.com <gt_min@naver.com>
2022-11-04 11:26:37 +0900

    2022-11-04

--- springstudy/07_Dbcp/src/test/java/com/gdu/app07/BoardUnitTest.java
+++ springstudy/07_Dbcp/src/test/java/com/gdu/app07/BoardUnitTest.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.gdu.app07.config.BoardAppContext;
 import com.gdu.app07.domain.BoardDTO;
 import com.gdu.app07.repository.BoardDAO;
 
 // JUnit4를 사용한다.
-@RunWith(SpringJUnit4ClassRunner.class)
+// 커스터마이징한 MySpringJUnit4ClassRunner를 사용한다.
+@RunWith(MySpringJUnit4ClassRunner.class)
 
 // Bean은 BoardAppContext에 정의되어 있다.
 @ContextConfiguration(classes={BoardAppContext.class})
