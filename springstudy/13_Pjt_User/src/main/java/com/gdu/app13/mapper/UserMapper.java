68fd1584cf7c96826e28a8506f7c45e19357ae5b
gt_min@naver.com <gt_min@naver.com>
2022-11-17 16:03:34 +0900

    2022-11-17

--- springstudy/13_Pjt_User/src/main/java/com/gdu/app13/mapper/UserMapper.java
+++ springstudy/13_Pjt_User/src/main/java/com/gdu/app13/mapper/UserMapper.java
@@ -18,5 +18,6 @@ public interface UserMapper {
 	public int deleteUser(int userNo);
 	public int insertRetireUser(RetireUserDTO retireUser);
 	public int updateSessionInfo(UserDTO user);
+	public int updateUserPassword(UserDTO user);
 	
 }
